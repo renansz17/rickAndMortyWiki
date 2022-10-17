f67974ce912323d145b9710291926e13dfd91617
Renan Szeremeta <renanszeremeta2@gmail.com>
2022-10-17 13:25:16 -0300

    add fixed header and pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { GlobalStyle } from './styles/global';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router';
 import "./index.css"
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Router />
       <GlobalStyle />
     </BrowserRouter>
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import { Characters } from "./pages/Characters";
+import { Episodes } from "./pages/Episodes";
 import { Home } from "./pages/Home";
+import { Locals } from "./pages/Locals";
 
 export function Router() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/characters" element={<Characters />} />
+            <Route path="/locals" element={<Locals />} />
+            <Route path="/episodes" element={<Episodes />} />
         </Routes>
     )
-}
-
-{/* <Route path="/" element={<DefaultLayout />}>
-    <Route path="/" element={<Home />} />
-    <Route path="/history" element={<History />} />
-</Route> */}
+};
