f33cc32f537bc0a2c1824526bd7a80cbafaa244a
Renan <renanszeremeta2@gmail.com>
2022-10-18 21:05:16 -0300

    fix

--- src/models/Character.ts
+++ src/models/Character.ts
@@ -1,26 +1,26 @@
 export interface Character {
-    id?: number;
-    name?: string;
-    status?: string,
-    species?: string,
-    type?: string,
-    gender?: string,
-    origin?: {
-        name?: string,
-        url?: string;
-    },
-    location: {
-        name?: string;
-        urel?: string;
-    },
-    image?: string;
-    episode: string[];
-    url?: string;
-    created?: string;
+  id?: number
+  name?: string
+  status?: string
+  species?: string
+  type?: string
+  gender?: string
+  origin?: {
+    name?: string
+    url?: string
+  }
+  location: {
+    name?: string
+    url?: string
+  }
+  image?: string
+  episode: string[]
+  url?: string
+  created?: string
 }
 
 export enum CharacterStatus {
-    ALIVE = "Alive",
-    DEAD = "Dead",
-    UNKNOWN = "Unknown"
+  ALIVE = 'Alive',
+  DEAD = 'Dead',
+  UNKNOWN = 'Unknown',
 }
