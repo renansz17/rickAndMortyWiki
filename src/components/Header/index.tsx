5bec25b9a1118f79f935477f223e46ddc7292d68
Renan <renanszeremeta2@gmail.com>
2022-10-30 20:30:25 -0300

    fix css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,31 +1,23 @@
-import React, { useState } from "react"
-import { NavLink } from "react-router-dom";
-import portalGif from '../../assets/images/portal.gif';
-import { HeaderContainer, NavigationContainer } from "./style";
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import portalGif from '../../assets/images/portal.gif'
+import { HeaderContainer, NavigationContainer } from './style'
 
 function Header() {
-    const [selectedOption, setSelectedOption] = useState();
-    const options = ['Personagens', 'Locais', 'Episódios'];    
-    return (
-        <HeaderContainer>
-            <NavigationContainer>
-                <NavLink to={"/"}>
-                    <img src={portalGif} alt="" />
-                </NavLink>
-                <div>
-                    <NavLink to={'/characters'}>
-                        Personagens
-                    </NavLink>
-                    <NavLink to={'/locals'}>
-                        Locais
-                    </NavLink>
-                    <NavLink to={'/episodes'}>
-                        Episódios
-                    </NavLink>
-                </div>
-            </NavigationContainer>
-        </HeaderContainer>
-    )
+  return (
+    <HeaderContainer>
+      <NavigationContainer>
+        <NavLink to={'/'}>
+          <img src={portalGif} alt="" />
+        </NavLink>
+        <div>
+          <NavLink to={'/characters'}>Personagens</NavLink>
+          <NavLink to={'/locals'}>Locais</NavLink>
+          <NavLink to={'/episodes'}>Episódios</NavLink>
+        </div>
+      </NavigationContainer>
+    </HeaderContainer>
+  )
 }
 
-export default Header;
+export default Header
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,24 +1,24 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
-    height: 4.375rem;
-    background-color: #121214;
-    padding: 1rem;
-    font-family: 'Get-Schwifty';
+  background-color: #121214;
+  padding: 1rem;
+  font-family: 'Get-Schwifty';
+  font-size: 1.5rem;
 `
 export const NavigationContainer = styled.nav`
-    display: flex;
-    justify-content: space-between;
-    font-size:18px;
+  display: flex;
+  justify-content: space-between;
+  font-size: 18px;
+  color: white;
+  text-decoration: none;
+
+  img {
+    width: 50px;
+  }
+  a {
+    margin: 0 5px;
     color: white;
     text-decoration: none;
-
-    img {
-        width: 50px;
-    }
-    a {
-        margin: 0 5px;
-        color: white;
-        text-decoration: none;
-    }
-`;
+  }
+`
