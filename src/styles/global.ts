e3357962e2f3b8a180fa33c957cf16aaeea0cf51
Renan <renanszeremeta2@gmail.com>
2022-10-30 20:31:07 -0300

    add rick and morty font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,10 +3,11 @@ import '../assets/fonts/get_schwifty.ttf'
 
 export const GlobalStyle = createGlobalStyle`
     * {
+       
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        font-family: 'Poppins', sans-serif;
         scroll-behavior: smooth;
+        overflow-x: hidden;
     }
 `
